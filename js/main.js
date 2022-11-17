814f76d2113de5c7360f68143602e76cfd5612ed
Your Name <iyanperezmarcos@gmail.com>
2022-11-17 18:20:47 +0100

    banner made responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 const back = document.querySelector("#back-button")
 const front = document.querySelector("#front-button")
 const slider = document.querySelector(".slider")
+const banner = document.querySelector(".banner")
+
 
 back.addEventListener("click", () =>{
-    slider.scrollLeft -= 910
+    slider.scrollLeft -= banner.offsetWidth
 })
 front.addEventListener("click", () =>{
-    slider.scrollLeft += 910
+    slider.scrollLeft += banner.offsetWidth
 })
+
+function resizingBannerImages(){
+    const bannerWidth=banner.offsetWidth 
+    const images=document.querySelectorAll(".banner-photo")
+
+    images.forEach(image => {
+        image.style.width = bannerWidth + "px"
+    })
+}
+
+new ResizeObserver(resizingBannerImages).observe(banner)
